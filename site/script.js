b630ee6235d9f11ee7b2945c11c1c3b371d83398
Seth Samuel <sethsml@hotmail.com>
2025-02-28 19:04:32 +1300

    added comments and moved drawing to its own function

--- site/script.js
+++ site/script.js
@@ -1,3 +1,5 @@
+// ----------------------- OBJECTS ---------------------- //
+
 class Note { //class for notes 
   constructor(tittle, text) { //takes title and note content 
     this.tittle = tittle;
@@ -5,6 +7,9 @@ class Note { //class for notes
     this.creationTime = new Date(); //sets the creation time at note creation
   }
 }
+
+// ------------------- HTML SELECTORS ------------------- //
+const mainDiv = document.getElementsByTagName("main")[0]; //get main element
 
 // https://stackoverflow.com/a/24676492
 // function auto_grow(element) {
@@ -18,57 +23,57 @@ notebook.push(new Note("tittle", "this is some text"));
 notebook.push(new Note("tittle again", "this is text is bomb"));
 notebook.push(new Note("3RD note", "this is the text for the 3rd note"));
 
-let mainDiv = document.getElementsByTagName("main")[0]; //get main element
 
 
-
-for (let x of notebook.values()) {
-  console.log(
-    "tittle: " +
-      x.tittle +
-      "\ncontents: " +
-      x.note +
-      "\ncreated on: " +
-      x.creationTime
-  );
-
-  console.log(JSON.stringify(x))
-
-  // div to hold the note
-  let div = document.createElement("div");
-  div.className = "note";
-  //note tittle
-  let h1 = document.createElement("h1");
-  h1.insertAdjacentText("afterbegin", x.tittle);
-  //note content
-  let content = document.createElement("p");
-  content.insertAdjacentText("afterbegin", x.note);
-  //div to hold creation date
-  let creationDateDiv = document.createElement("div");
-  creationDateDiv.className = "creationDate";
-  //label for creation date
-  let labelForCreationDate = document.createElement("p");
-  labelForCreationDate.insertAdjacentText("afterbegin", "Creation Date: ");
-  //creation date
-  let creationDate = document.createElement("p");
-  creationDate.insertAdjacentText("afterbegin", x.creationTime);
-
-  div.append(h1);
-
-  div.append(content);
-  creationDateDiv.append(labelForCreationDate);
-  creationDateDiv.append(creationDate);
-  div.append(creationDateDiv);
-  mainDiv.append(div);
-}
 
 const newNoteDialog = document.querySelector("dialog");
 const newNoteButton = document.getElementById("newNoteButton");
 newNoteButton.addEventListener("click", () => {
 newNoteDialog.showModal();
 });
-// mainDiv = document.getElementById("main");
 
-// div.innerHTML="Hello World";
-// mainDiv.append(div);
+function draw(){
+  for (let note of notebook.values()) {
+    console.log(
+      "tittle: " +
+        note.tittle +
+        "\ncontents: " +
+        note.note +
+        "\ncreated on: " +
+        note.creationTime
+    );
+  
+    console.log(JSON.stringify(note))
+  
+// ------------------- CREATE NOTE IN HTML ------------------ //
 
+    // div to hold the note
+    const div = document.createElement("div");
+    div.className = "note";
+    //note tittle
+    const h1 = document.createElement("h1");
+    h1.insertAdjacentText("afterbegin", note.tittle);
+    //note content
+    const content = document.createElement("p");
+    content.insertAdjacentText("afterbegin", note.note);
+    //div to hold creation date
+    const creationDateDiv = document.createElement("div");
+    creationDateDiv.className = "creationDate";
+    //label for creation date
+    const labelForCreationDate = document.createElement("p");
+    labelForCreationDate.insertAdjacentText("afterbegin", "Creation Date: ");
+    //creation date
+    const creationDate = document.createElement("p");
+    creationDate.insertAdjacentText("afterbegin", note.creationTime);
+  
+    //append everything in order
+    div.append(h1);  
+    div.append(content);
+    creationDateDiv.append(labelForCreationDate);
+    creationDateDiv.append(creationDate);
+    div.append(creationDateDiv);
+    mainDiv.append(div);
+  }
+}
+
+draw(); //draw the webpage on first load
