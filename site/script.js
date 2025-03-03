efd851715b128e5b39635ccd55505e10f5d508fc
Seth Samuel <sethsml@hotmail.com>
2025-03-03 20:55:49 +1300

    edit note works

--- site/script.js
+++ site/script.js
@@ -1,21 +1,18 @@
 // ------------------------ DEBUG ----------------------- //
-//uncomment to turn off console logs 
-// console.log = (x)=>{} 
-
+//uncomment to turn off console logs
+// console.log = (x)=>{}
 
 // ----------------------- OBJECTS ---------------------- //
 
 class Note {
   //class for notes
-  constructor(title, text) {
+  constructor(title, text, creationTime=new Date()) {
     //takes title and note content
     this.title = title;
     this.note = text;
-    this.creationTime = new Date(); //sets the creation time at note creation
+    this.creationTime = creationTime; //sets the creation time at note creation
   }
 }
-
-
 
 // https://stackoverflow.com/a/24676492
 // function auto_grow(element) {
@@ -35,41 +32,46 @@ const mainDiv = document.getElementsByTagName("main")[0]; //get main element
 const newNoteDialog = document.querySelector("#newNoteDialog"); //get dialog element for new notes
 const newNoteButton = document.getElementById("newNoteButton"); // new note button on main page
 const newNoteTitle = document.querySelector("#newNoteDialog input"); //new note title in new note dialog
-const newNoteContents = document.querySelector("#newNoteDialog textarea") //new note contents in new note dialog
-const newNoteDialogSaveButton = document.querySelector("#noteEditSaveButton");//save button in new note dialog 
+const newNoteContents = document.querySelector("#newNoteDialog textarea"); //new note contents in new note dialog
+const newNoteDialogSaveButton = document.querySelector("#noteEditSaveButton"); //save button in new note dialog
 const sortButton = document.querySelector("#sortForm input"); //sort button on main page
-const sortDirection = document.querySelector("#sortBySelect") //sort type dropdown 
-const noteEditCancelButton = document.querySelector("#noteEditCancelButton")//cancel button in note edit dialog
+const sortDirection = document.querySelector("#sortBySelect"); //sort type dropdown
+const noteEditCancelButton = document.querySelector("#noteEditCancelButton"); //cancel button in note edit dialog
 
 // ---------------------- FUNCTIONS ---------------------- //
 
-function editNote(e){
+function editNote(e) {
   e.preventDefault();
   // console.log(e.target)
   // console.log(e.target.dataset.id)
   const key = new Date(); //create a date object to use as my key to access indexedDB
-  key.setTime(e.target.dataset.id) //set my key to the same time as the note
-  console.log(key)
-  const request = db.transaction(objectStoreNameNotes,"readonly").objectStore(objectStoreNameNotes).get(key); //get note from store
+  key.setTime(e.target.dataset.id); //set my key to the same time as the note
+  console.log(key);
+  const request = db
+    .transaction(objectStoreNameNotes, "readonly")
+    .objectStore(objectStoreNameNotes)
+    .get(key); //get note from store
+
+
+  newNoteDialog.dataset.NoteId = e.target.dataset.id //set the id so we can replace the notes
   
-  request.onerror = (e)=>{
-    alert(e)
-  }
+  request.onerror = (e) => {
+    alert(e);
+  };
 
-  request.onsuccess = (e) =>{ //set note edit dialog to the content of the note and show it. 
+  request.onsuccess = (e) => {
+    //set note edit dialog to the content of the note and show it.
     const oldNote = e.target.result;
-    console.log(e.target.result)
-    newNoteTitle.value =oldNote.title;
+    console.log(e.target.result);
+    newNoteTitle.value = oldNote.title;
     newNoteContents.value = oldNote.note;
+    newNoteDialog.dataset.editNote = true; //change the behavior of the new note dialog
     newNoteDialog.showModal();
-  }
-
+  };
 }
 
-
 function draw() {
-
-  mainDiv.innerHTML=""; //clear main div for redraw 
+  mainDiv.innerHTML = ""; //clear main div for redraw
 
   for (let note of notebook.values()) {
     // console.log(
@@ -102,25 +104,28 @@ function draw() {
     labelForCreationDate.insertAdjacentText("afterbegin", "Creation Date: ");
     //creation date
     const creationDate = document.createElement("p");
-    creationDate.insertAdjacentText("afterbegin", note.creationTime.toLocaleString());
+    creationDate.insertAdjacentText(
+      "afterbegin",
+      note.creationTime.toLocaleString()
+    );
 
     //div to hold edit and delete buttons
-    const editAndDeleteButtonClassName = "noteButtons"
+    const editAndDeleteButtonClassName = "noteButtons";
     const editAndDeleteDiv = document.createElement("div");
-    editAndDeleteDiv.classList.add("editAndDeleteDiv")
-    //edit note button 
+    editAndDeleteDiv.classList.add("editAndDeleteDiv");
+    //edit note button
     const editNoteButton = document.createElement("button");
     editNoteButton.classList.add("editNoteButton");
-    editNoteButton.classList.add(editAndDeleteButtonClassName)
-    editNoteButton.insertAdjacentText("afterbegin","Edit");
+    editNoteButton.classList.add(editAndDeleteButtonClassName);
+    editNoteButton.insertAdjacentText("afterbegin", "Edit");
     editNoteButton.dataset.id = note.creationTime.getTime(); //add the creation time as an id so we can find the note
-    editNoteButton.addEventListener("click",editNote) 
-    editAndDeleteDiv.append(editNoteButton)
+    editNoteButton.addEventListener("click", editNote);
+    editAndDeleteDiv.append(editNoteButton);
     //delete note button
     const deleteNoteButton = document.createElement("button");
     deleteNoteButton.classList.add("deleteNoteButton");
-    deleteNoteButton.insertAdjacentText("afterbegin","DELETE");
-    deleteNoteButton.classList.add(editAndDeleteButtonClassName)
+    deleteNoteButton.insertAdjacentText("afterbegin", "DELETE");
+    deleteNoteButton.classList.add(editAndDeleteButtonClassName);
     editAndDeleteDiv.append(deleteNoteButton);
 
     //append everything in order
@@ -129,117 +134,143 @@ function draw() {
     creationDateDiv.append(labelForCreationDate);
     creationDateDiv.append(creationDate);
     div.append(creationDateDiv);
-    div.append(editAndDeleteDiv)
+    div.append(editAndDeleteDiv);
     mainDiv.append(div);
   }
 }
 
-function connectToDB(){ //connect to indexedDB for saving and loading notes
-  const openRequest = window.indexedDB.open(dbName,1);
+function connectToDB() {
+  //connect to indexedDB for saving and loading notes
+  const openRequest = window.indexedDB.open(dbName, 1);
 
-  openRequest.addEventListener("error",()=>{
-    console.log("IndexedDB failed to open")
+  openRequest.addEventListener("error", () => {
+    console.log("IndexedDB failed to open");
   });
 
-  openRequest.addEventListener("success",()=>{ //connects successfully with out issue
-    
+  openRequest.addEventListener("success", () => {
+    //connects successfully with out issue
+
     db = openRequest.result; //so we can use the IndexedDB later
-    loadFromDB()
-    draw()
-    console.log("IndexedDB opened successfully")
+    loadFromDB();
+    draw();
+    console.log("IndexedDB opened successfully");
   });
 
-  openRequest.addEventListener("upgradeneeded",(e)=>{ //connects but db is either not created or needs to be updated
+  openRequest.addEventListener("upgradeneeded", (e) => {
+    //connects but db is either not created or needs to be updated
     db = e.target.result;
-    const objectStore = db.createObjectStore(objectStoreNameNotes,{keypath: "creationTime"}) //create an object store with the object store name
+    const objectStore = db.createObjectStore(objectStoreNameNotes, {
+      keypath: "creationTime",
+    }); //create an object store with the object store name
 
-    objectStore.createIndex("note","note"); //create an index in the object store called "note" to be accessed the param "IDBCursorObject.value"
+    objectStore.createIndex("note", "note"); //create an index in the object store called "note" to be accessed the param "IDBCursorObject.value"
 
-    console.log("IndexedDB setup complete")
+    console.log("IndexedDB setup complete");
     draw(); //draw webpage
-  })
+  });
 }
 
-function loadFromDB(){ //load data from IndexedDB
-  const objectStore = db.transaction(objectStoreNameNotes).objectStore(objectStoreNameNotes);
-  objectStore.openCursor().addEventListener("success",(e)=>{
+function loadFromDB() {
+  //load data from IndexedDB
+  const objectStore = db
+    .transaction(objectStoreNameNotes)
+    .objectStore(objectStoreNameNotes);
+  objectStore.openCursor().addEventListener("success", (e) => {
     const cursor = e.target.result;
-    console.log("loading from IndexedDB")
+    console.log("loading from IndexedDB");
 
-    if(cursor){
+    if (cursor) {
       notebook.push(cursor.value);
-      console.log(cursor.value.creationTime)
+      console.log(cursor.value.creationTime);
       cursor.continue();
-    } 
-    draw()
-    console.log("Draw from loadDB")
-  })
-
+    }
+    draw();
+    console.log("Draw from loadDB");
+  });
 }
 
-function saveToDB(newNote){
-  const transaction = db.transaction([objectStoreNameNotes],"readwrite"); //set up a transaction to handle the status events 
-  const objectStore = transaction.objectStore(objectStoreNameNotes); //specify the object store to access 
-  const addRequest = objectStore.add(newNote,newNote.creationTime); //use the transaction to generate a request to add the new note to the IndexedDB
+function saveToDB(newNote) {
+  const transaction = db.transaction([objectStoreNameNotes], "readwrite"); //set up a transaction to handle the status events
+  const objectStore = transaction.objectStore(objectStoreNameNotes); //specify the object store to access
+  const addRequest = objectStore.add(newNote, newNote.creationTime); //use the transaction to generate a request to add the new note to the IndexedDB
 
-  transaction.addEventListener("complete",()=>{
+  transaction.addEventListener("complete", () => {
     console.log(`Note: ${newNote.title} saved to IndexedDB`);
-  })
+  });
 
-  transaction.addEventListener("error",()=>{
-    alert(`Note: ${newNote.title} not saved.\nYou will loose the note on browser refresh`)
-  })
-
+  transaction.addEventListener("error", () => {
+    alert(
+      `Note: ${newNote.title} not saved.\nYou will loose the note on browser refresh`
+    );
+  });
 }
 
 // -------------------GLOBAL EVENT LISTENERS ------------------ //
 
-newNoteButton.addEventListener("click", () => { //open the new note modal
+newNoteButton.addEventListener("click", () => {
+  //open the new note modal
   newNoteTitle.value = ""; //clear title on modal open
-  newNoteContents.value = "" //clear text area on modal open
+  newNoteContents.value = ""; //clear text area on modal open
   newNoteDialog.showModal();
 });
 
-newNoteDialogSaveButton.addEventListener("click", (e) =>{ //button to save the note in the new note modal
+newNoteDialogSaveButton.addEventListener("click", (e) => {
+  //button to save the note in the new note modal
   // e.preventDefault();//stop the window closing if the note doesnt save to IndexedDB
-  const tempNote = new Note(newNoteTitle.value,newNoteContents.value);
-  notebook.push(tempNote); //save note into working array
-  saveToDB(tempNote); //save temp note into IndexedDB
-  draw()
+  if (newNoteDialog.dataset.editNote) {
+    const creationDate  = new Date();
+    creationDate.setTime(newNoteDialog.dataset.NoteId);
+    const tempNote = new Note(newNoteTitle.value,newNoteContents.value,creationDate)
+    tempNote.modTime = new Date();
+    const updateRequest = db.transaction(objectStoreNameNotes,"readwrite").objectStore(objectStoreNameNotes).put(tempNote,creationDate)
+    notebook.length = 0; // clears the array https://stackoverflow.com/a/1232046 
+    loadFromDB(); 
+    newNoteDialog.dataset.editNote=false;
+    newNoteDialog.dataset.NoteId= 0;
+
+  } else {
+    const tempNote = new Note(newNoteTitle.value, newNoteContents.value);
+    notebook.push(tempNote); //save note into working array
+    saveToDB(tempNote); //save temp note into IndexedDB
+    draw();
+  }
 });
 
 // sortButton.addEventListener("click",()=>{ //sorting functions
-sortDirection.addEventListener("click",()=>{
+sortDirection.addEventListener("click", () => {
+  switch (sortDirection.value) {
+    case "a-z":
+      notebook.sort((a, b) => {
+        //sort by Title A-Z
+        return a.title.localeCompare(b.title);
+      });
+      break;
+    case "z-a":
+      notebook.sort((a, b) => {
+        //sort by Title Z-A
+        return b.title.localeCompare(a.title);
+      });
+      break;
+    case "newest":
+      notebook.sort((a, b) => {
+        //sort by newest note first
+        console.log(b.creationTime - a.creationTime);
+        return b.creationTime - a.creationTime; //"b" goes first because the Date object stores the time as Unix time. thus an object created later will have a bigger number. since we want the biggest "time value" to be first a bigger number minus a smaller number will create a positive value meaning when it is returned "a" will be placed ahead of "b" .
+      });
+      break;
+    case "oldest":
+      notebook.sort((a, b) => {
+        return a.creationTime - b.creationTime; //opposite of above
+      });
+      break;
+  }
+  draw();
+});
 
-  switch (sortDirection.value){
-  case "a-z": notebook.sort((a,b)=>{ //sort by Title A-Z
-    return a.title.localeCompare(b.title);
-  }) ;
-  break;
-  case "z-a": notebook.sort((a,b)=>{ //sort by Title Z-A
-    return b.title.localeCompare(a.title) 
-  });
-  break;
-  case "newest": notebook.sort((a,b)=>{ //sort by newest note first
-    console.log(b.creationTime - a.creationTime)
-    return (b.creationTime - a.creationTime)//"b" goes first because the Date object stores the time as Unix time. thus an object created later will have a bigger number. since we want the biggest "time value" to be first a bigger number minus a smaller number will create a positive value meaning when it is returned "a" will be placed ahead of "b" .
-  })
-  break;
-  case "oldest": notebook.sort((a,b)=>{
-    return a.creationTime - b.creationTime; //opposite of above 
-  })
-  break;
-
-}
-  draw()
-})
-
-noteEditCancelButton.addEventListener("click",(e)=>{
-  e.preventDefault()// stop the form from being submitted
+noteEditCancelButton.addEventListener("click", (e) => {
+  e.preventDefault(); // stop the form from being submitted
   newNoteDialog.close();
-})
-
-
+});
 
 // ------------------ SCRIPT ENTRYPOINT ----------------- //
 
@@ -249,5 +280,5 @@ noteEditCancelButton.addEventListener("click",(e)=>{
 // notebook.push(new Note("3RD note", "this is the text for the 3rd note"));
 
 //set fonts
-document.querySelector("body").className= "noto-serif-georgian-body";
+document.querySelector("body").className = "noto-serif-georgian-body";
 connectToDB();
